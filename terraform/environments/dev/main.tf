16b584a896eaf3a380b7648feb686fca0c93b65b
suriyamsjfrog <suriyams@jfrog.com>
2026-03-26 12:37:29 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,3 +1,17 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_security_group" "bad_sg" {
+  name = "allow_all_traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # ❌ Open to the world
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
