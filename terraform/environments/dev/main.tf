b90ace8a8f4be9df8d76eb5f62a8aa5f35722a04
suriyamsjfrog <suriyams@jfrog.com>
2026-03-26 12:49:18 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,17 +1,10 @@
 resource "aws_security_group" "bad_sg" {
-  name = "allow_all_traffic"
+  name = "ssh_open"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # ❌ Open to the world
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # 🚨 SSH open to world
   }
 }
